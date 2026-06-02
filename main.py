d1f7db22bfd8ebf6196841257578cb5fc0f6608f
Isak Bjerkem <isakbjerkem@gmail.com>
2026-06-02 15:11:44 +0200

    added gameloop, fill screen and quit event

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
+from logger import log_state
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    
+    while running:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+        
+        screen.fill("black")
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
