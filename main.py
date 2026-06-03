5f252ece05851853ba9426144a26133e7fb0198c
Isak Bjerkem <isakbjerkem@gmail.com>
2026-06-03 13:30:08 +0200

    added more constants, and updated the main gameloop to use players, and drawables and updatables

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
 from logger import log_state
-
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0 
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
         log_state()
 
@@ -16,10 +25,15 @@ def main():
                 running = False 
         
         screen.fill("black")
+        dt = clock.tick(60) / 1000.0
+        
+        
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        pygame.display.flip()
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
